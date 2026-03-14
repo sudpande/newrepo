91a03ef1d402d41bb08909586acc4ceb22c7668e
Sudhir Pandey <sudhir.iiit@gmail.com>
2026-03-14 17:26:33 +0100

    Add function to sum two numbers

---
+++ Second-programme.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    return a + b
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = add_numbers(num1, num2)
+print("The sum is:", result)
+
